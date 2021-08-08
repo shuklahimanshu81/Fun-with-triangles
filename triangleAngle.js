e210972092ea8cc600e9e0d7628f2962d2f85bd4
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2021-08-09 00:08:33 +0530

    initial commit

---
+++ triangleAngle.js
@@ -0,0 +1,25 @@
+var angle1 = document.querySelector("#angle1")
+var angle2 = document.querySelector("#angle2")
+var thirdAngleBtn = document.querySelector("#third-angle");
+var outputDiv = document.querySelector("#outputDiv");
+
+
+thirdAngleBtn.addEventListener("click", clickHandler);
+
+
+function clickHandler(){
+    
+    if(Number(angle1.value)<=0 || Number(angle2.value)<=0){
+        outputDiv.innerText = "Looks like you enterd invalid value of Angles. "
+        return;
+    }
+   
+    
+    if((Number(angle1.value) + Number(angle2.value))>179){
+        outputDiv.innerText ="Looks like Angles you entered do not form a Triangle."
+        return;
+    }
+  if(Number(angle1.value)>0 && Number(angle2.value)>0){
+     outputDiv.innerText = "Value of third angle of the triangle is " + (180-(Number(angle1.value) + Number(angle2.value)))
+  }
+}
