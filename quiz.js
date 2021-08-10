72c5ae2a33d49cb5a22f8217adca47dff72dcb6e
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2021-08-10 20:24:12 +0530

    refactor

--- quiz.js
+++ quiz.js
@@ -1,29 +1,26 @@
-
-
 var formQuiz = document.querySelector("#quiz-form");
 var submitBtn = document.querySelector("#submit-btn");
 var outputDiv = document.querySelector("#outputDiv");
 
-var answers = [ "Equilateral" , "Median", "No", "Yes", "36" , "10"];
+var answers = ["Equilateral", "Median", "No", "Yes", "36", "10"];
 
-submitBtn.addEventListener("click",clickHandler);
+submitBtn.addEventListener("click", clickHandler);
 
+function clickHandler() {
+  
+    var i = 0;
+  var score = 0;
 
-function clickHandler(){
-   
-    var i =0;
-    var score = 0;
-    var userAnswer = new FormData(formQuiz);
+  var userAnswer = new FormData(formQuiz);
 
-    for(const value of userAnswer.values()){
-        if(value === answers[i]){
-            score = score+1;
-        }
-        i++;
+  for (const value of userAnswer.values()) {
+    if (value === answers[i]) {
+      score = score + 1;
     }
+    i++;
+   
+  }
 
-    outputDiv.innerText = "Your score is " + score;
-    submitBtn.style.display = "none;"
+  outputDiv.innerText = "Your score is " + score;
 }
-
 
