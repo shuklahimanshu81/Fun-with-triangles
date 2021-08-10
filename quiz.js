8045af4961813a405dd65de51da72f30d3ab8894
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2021-08-10 22:45:01 +0530

    styling : border

--- quiz.js
+++ quiz.js
@@ -1,27 +1,27 @@
 const formQuiz = document.forms[0];
 const submitBtn = document.querySelector("#submit-btn");
 const outputDiv = document.querySelector("#outputDiv");
-const questionBox = document.querySelectorAll("#qustion-box")
+const questionBox = document.querySelectorAll("#question-box");
+
 const answers = ["Equilateral", "Median", "No", "Yes", "36", "10"];
 
-
+let score = 0;
 
 formQuiz.addEventListener('submit', (e) => {
     e.preventDefault();
   
     let i = 0;
-    let score = 0;
+    
   
   const userAnswer = new FormData(formQuiz);
 
   for (let value of userAnswer.values()) {
     if (value == answers[i]) {
+      questionBox[i].style.backgroundColor = "skyblue"
       score = score + 1;
-      questionBox.style.backgroundColor = "skyblue"
-
     }
     else{
-        questionBox.style.backgroundColor = "lightYellow"
+        questionBox[i].style.backgroundColor = "lightYellow"
     }
     i++;
    
