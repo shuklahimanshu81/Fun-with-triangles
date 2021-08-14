82db930a37abd9756684020ee39d36a0596a1f4b
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2021-08-15 02:42:07 +0530

    feat: Change in Triange angles

--- triangleAngle.js
+++ triangleAngle.js
@@ -1,5 +1,6 @@
 var angle1 = document.querySelector("#angle1")
 var angle2 = document.querySelector("#angle2")
+var angle3 = document.querySelector("#angle3")
 var thirdAngleBtn = document.querySelector("#third-angle");
 var outputDiv = document.querySelector("#outputDiv");
 
@@ -9,17 +10,17 @@ thirdAngleBtn.addEventListener("click", clickHandler);
 
 function clickHandler(){
     
-    if(Number(angle1.value)<=0 || Number(angle2.value)<=0){
+    if(Number(angle1.value)<=0 || (Number(angle2.value)<=0 || Number(angle3.value)<=0)){
         outputDiv.innerText = "Looks like you enterd invalid value of Angles. "
         return;
     }
    
     
-    if((Number(angle1.value) + Number(angle2.value))>179){
-        outputDiv.innerText ="Looks like Angles you entered do not form a Triangle."
+    if((Number(angle1.value) + Number(angle2.value) + Number(angle3.value)) === 180){
+        outputDiv.innerText ="Yeah! These Angles form a Triangle."
         return;
     }
-  if(Number(angle1.value)>0 && Number(angle2.value)>0){
-     outputDiv.innerText = "Value of third angle of the triangle is " + (180-(Number(angle1.value) + Number(angle2.value)))
-  }
+   else{
+    outputDiv.innerText =" Oops! Looks like these Angles don not form a Triangle."
+   }
 }
